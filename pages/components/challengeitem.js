081c0a24d20e7dbd3dd3c505eafba56e94d9496d
Jaeheon Shim <git@jaeheonshim.com>
2021-09-17 19:19:37 -0400

    Get home page challenges from server

--- pages/components/challengeitem.js
+++ pages/components/challengeitem.js
@@ -1,11 +1,11 @@
 import styles from "../../styles/home.module.css"
 
-export default function ChallengeItem() {
+export default function ChallengeItem(props) {
     return (
         <div className={styles.challenge}>
             <div className={styles["challenge-details"]}>
-                <h3>Challenge #1</h3>
-                <p>Greatest common multiple</p>
+                <h3>{props.name}</h3>
+                <p>{props.shortdesc}</p>
             </div>
             <div>
                 <button>Solve!</button>
--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/home.module.css"
 import ChallengeItem from "./components/challengeitem";
 
 export default function Home() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [challengeList, setChallengeList] = useState();
+
+    useEffect(() => {
+        fetch("/api/challenges")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);  
+                    setChallengeList(result.map((e) => <ChallengeItem name={e.name} />)); 
+                }
+            );
+    }, []);
+
     return (
         <div>
             <h1>Welcome, Jaeheon</h1>
@@ -9,9 +24,9 @@ export default function Home() {
                 <div>
                     <h2>Available Challenges</h2>
                     <div className={styles["flex-list"]}>
-                        <ChallengeItem />
-                        <ChallengeItem />
-                        <ChallengeItem />
+                        {
+                            !isLoaded ? "Loading..." : challengeList
+                        }
                     </div>
                 </div>
                 <div>
